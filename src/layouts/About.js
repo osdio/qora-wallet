66045cf95b5e26eef8aff7f96eb5a612ed58ccd8
soliury <lingyong1992@foxmail.com>
2016-02-22 17:01:34 +0800

    delete some file

--- src/layouts/About.js
+++
@@ -1,9 +0,0 @@
-import About from '../components/About';
-
-export function mapStateToProps(state) {
-    return {
-        home: state.home
-    };
-}
-
-export const component = About;
--- src/layouts/MyDatePickerIOS.js
+++
@@ -1,7 +0,0 @@
-import MyDatePickerIOS from '../components/MyDatePickerIOS';
-
-export function mapStateToProps () {
-    return {};
-}
-
-export const component = MyDatePickerIOS;
