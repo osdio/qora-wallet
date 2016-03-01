e8c493548b0e58d43b803e441cc3b215a8405296
soliury <lingyong1992@foxmail.com>
2016-03-01 21:50:01 +0800

    remove rn-cli.config

--- rn-cli.config.js
+++
@@ -1,7 +0,0 @@
-var blacklist= require("react-native/packager/blacklist");
-var config = {
-    getBlacklistRE(platform) {
-        return blacklist(platform,[/qora-wallet.+\/node_modules\/crypto.*/]);
-    }
-}
-module.exports = config;
